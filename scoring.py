92dfd0ccc02d1f166f4e08df1473c1028ddce247
ojh31 <oskar@far.ai>
2024-08-08 13:14:00 -0700

    Scored using summary too

--- scoring.py
+++ scoring.py
@@ -3,10 +3,13 @@ from feedparser import FeedParserDict
 
 def score_entry(entry: FeedParserDict, config: dict) -> int:
     score = config["scoring"]["bias"]
-    for title_dict in config["scoring"]["titles"]:
-        if title_dict["title"] in entry.title.lower():
-            assert "value" in title_dict, f"Value not found in title_dict: {title_dict}"
-            score += title_dict["value"]
+    for word_dict in config["scoring"]["words"]:
+        if word_dict["word"] in entry.title.lower():
+            assert "value" in word_dict, f"Value not found in word_dict: {word_dict}"
+            score += word_dict["value"]
+        if word_dict["word"] in entry.summary.lower():
+            assert "value" in word_dict, f"Value not found in word_dict: {word_dict}"
+            score += word_dict["value"] * config["scoring"]["summary_multiplier"]
     for author_dict in config["scoring"]["authors"]:
         for entry_author in entry.authors:
             if author_dict["author"] in entry_author.name:
@@ -15,4 +18,4 @@ def score_entry(entry: FeedParserDict, config: dict) -> int:
         entry_tags = [t.term for t in entry.tags]
         if tag_dict["tag"] in entry_tags:
             score += tag_dict["value"]
-    return score
+    return int(score)
